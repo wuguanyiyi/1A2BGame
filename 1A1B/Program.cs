086d9a36882a04a01a17e020c39c16a881fa3780
wuguanyiyi <wuguanguanyi@gmail.com>
2023-02-14 17:01:32 +0800

    完成

--- 1A1B/Program.cs
+++ 1A1B/Program.cs
@@ -14,17 +14,18 @@ namespace _1A1B
             {
                 string[] arryRndnum = new string[4]; //亂數陣列
                 string[] plNum = new string[4]; //猜數字陣列
+
                 Random rand = new Random(); //隨機亂數
                 for(int i = 0; i < 4; i++) //隨機亂數跑四次
                 {
-                    arryRndnum[i]=Convert.ToString(rand.Next(0,9));//共10個數字
+                    arryRndnum[i]=Convert.ToString(rand.Next(0,9));//共10個數字，字串轉陣列
                     if (arryRndnum[0] == "0")//第一個亂數=0 (不能=0)
                     {
-                        arryRndnum[0]= Convert.ToString(rand.Next(1,9));//???
+                        arryRndnum[0]= Convert.ToString(rand.Next(1,9));//跑1-10隨機亂數，字串轉陣列
                     }
-                    for(int j = 0; j < i; j++)//
+                    for(int j = 0; j < i; j++)//arryRndnum[0] != "0"跑迴圈 
                     {
-                        if (arryRndnum[j]== arryRndnum[i])
+                        if (arryRndnum[j]== arryRndnum[i]) 
                         {
                             j = 0;
                             arryRndnum[i] =Convert.ToString(rand.Next(0,9));
@@ -32,13 +33,14 @@ namespace _1A1B
                     }
                     Console.WriteLine(arryRndnum[i]);
                 }
+
                 Console.WriteLine("歡迎來到1A1B猜數字遊戲");
-                for(int k = 1; k != 0; k++)
+                for(int k = 1; k != 0; k++)//玩家輸入，次數不限
                 {
-                    int a = 0, b = 0;
+                    int a = 0, b = 0;//宣告int型態的a、b變數
                     Console.WriteLine("---");
                     Console.WriteLine("請輸入不重複的4個數字");
-                    string playNum =Console.ReadLine();
+                    string playNum =Console.ReadLine();//讀取玩家輸入的四個數字
                     Console.WriteLine(playNum);
                     for(int j = 0; j < 4; j++)
                     {
@@ -48,15 +50,15 @@ namespace _1A1B
                     {
                         for(int j = 0; j < 4; j++)
                         {
-                            if (playNum[j]== playNum[i])
+                            if (plNum[j] == arryRndnum[i])
                             {
                                 if (i == j)
                                 {
-                                    a++;
+                                    a = a + 1;
                                 }
                                 else
                                 {
-                                    b++;
+                                    b = b + 1;
                                 }
                             }
                         }
@@ -68,11 +70,12 @@ namespace _1A1B
                         break;
                     }
                 }
+
                 Console.WriteLine("是否要繼續遊玩?(yes/no)");
-                string yn = Console.ReadLine();
-                if (yn == "no")
+                string yn = Console.ReadLine();//讀取輸入
+                if (yn == "no")//如果輸入no
                 {
-                    break;
+                    break;//跳出最外框迴圈程式
                 }
             }
             Console.ReadKey();
---
+++ 自己寫1A1B/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 自己寫1A1B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1A1B猜數字
+
+            // 
+            string[] arrRnd = new string[4];//宣告一個arrRnd陣列
+            Random rnd = new Random();//隨機亂數
+            for(int i = 0; i < 4; i++)//亂數跑四次
+            {
+                if(i== 0)
+                arrRnd[i] = Convert.ToString(rnd.Next(0,9));//10個數字，字串轉換成陣列
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
